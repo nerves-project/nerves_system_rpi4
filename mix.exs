25ef97ae73752834f2148bac9a48282d5b34b5b9
Eric Rauer <github@rauer.dev>
2020-12-08 14:31:28 -0800

    Upgrade nerves br version

--- mix.exs
+++ mix.exs
@@ -56,8 +56,8 @@ defmodule LY11SystemRpi4.MixProject do
 
   defp deps do
     [
-      {:nerves, "~> 1.5.4 or ~> 1.6.0 or ~> 1.7.0", runtime: false},
-      {:nerves_system_br, "1.13.4", runtime: false},
+      {:nerves, "~> 1.5.4 or ~> 1.6.0 or ~> 1.7.1", runtime: false},
+      {:nerves_system_br, "1.13.5", runtime: false},
       {:nerves_toolchain_aarch64_unknown_linux_gnu, "~> 1.3.0", runtime: false},
       {:nerves_system_linter, "~> 0.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.22", only: :docs, runtime: false}
