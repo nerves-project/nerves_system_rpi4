3ce9d03d34a65d6513c3497a1340760bf7cfa359
Jaremy J. Creechley <jaremy.creechley@wavebaselabs.com>
2021-10-11 02:05:27 -0700

    testing mipi

---
+++ packages/mipi_dsi_ili9881d/mipi_dsi.mod
@@ -0,0 +1,2 @@
+/home/elcritch/tools/nerves-systems/seeed-linux-dtoverlays/modules/mipi_dsi/mipi_dsi_drv.o /home/elcritch/tools/nerves-systems/seeed-linux-dtoverlays/modules/mipi_dsi/touch_panel.o /home/elcritch/tools/nerves-systems/seeed-linux-dtoverlays/modules/mipi_dsi/panel-ili9881d.o
+
---
+++ packages/mipi_dsi_ili9881d/mipi_dsi.mod.c
@@ -0,0 +1,31 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+MODULE_INFO(depends, "drm");
+
+MODULE_ALIAS("of:N*T*Ci2c_dsi,ili9881d");
+MODULE_ALIAS("of:N*T*Ci2c_dsi,ili9881dC*");
+
+MODULE_INFO(srcversion, "9ACAB9931CA2B526F163322");
