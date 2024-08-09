6e027d0f54082131f4a2ac0ae01f60ae692fab96
Jaremy Creechley <creechley@gmail.com>
2024-08-09 01:02:15 -0600

    update

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule NervesSystemRpi4.MixProject do
   use Mix.Project
 
   @github_organization "elcritch"
-  @app :nerves_system_rpi4
+  @app :container_rpi4
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
