f0e4f874323c6275405c2ea8b1fd08107039ccc2
Lars Wikman <lars@underjord.io>
2024-08-28 12:06:55 +0200

    Try an artifact :)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NervesSystemRpi4.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
+  @github_organization "lawik"
   @app :nerves_system_rpi4
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
